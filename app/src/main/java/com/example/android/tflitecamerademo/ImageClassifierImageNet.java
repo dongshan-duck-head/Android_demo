30d0874c7180ca82ec1c6917e75607839dd77a2b
kidneyweakx <kidneyweakx@gmail.com>
2019-04-09 23:55:20 +0800

    change mobilenet tflite and work

--- app/src/main/java/com/example/android/tflitecamerademo/ImageClassifierImageNet.java
+++ app/src/main/java/com/example/android/tflitecamerademo/ImageClassifierImageNet.java
@@ -6,19 +6,13 @@ import java.io.IOException;
 
 public class ImageClassifierImageNet extends ImageClassifier {
     /**
-     * The inception net requires additional normalization of the used input.
-     */
-    private static final int IMAGE_MEAN = 128;
-    private static final float IMAGE_STD = 128.0f;
-
-    /**
-     * An array to hold inference results, to be feed into Tensorflow Lite  outputs.
-     * This isn't part of the super class, because we need a primitive array ashere.
+     * An array to hold inference results, to be feed into Tensorflow Lite as outputs. This isn't part
+     * of the super class, because we need a primitive array here.
      */
     private float[][] labelProbArray = null;
 
     /**
-     * Initializes an {@code ImageClassifier}.
+     * Initializes an {@code ImageClassifierFloatMobileNet}.
      *
      * @param activity
      */
@@ -30,36 +24,36 @@ public class ImageClassifierImageNet extends ImageClassifier {
     @Override
     protected String getModelPath() {
         // you can download this file from
-        // https://storage.googleapis.com/download.tensorflow.org/models/tflite/inception_v3_slim_2016_android_2017_11_10.zip
-        return "model.tflite";
+        // see build.gradle for where to obtain this file. It should be auto
+        // downloaded into assets.
+        return "retrained.tflite";
     }
 
     @Override
     protected String getLabelPath() {
-        return "labels.txt";
+        return "retrained_labels.txt";
     }
 
     @Override
     protected int getImageSizeX() {
-        return 299;
+        return 224;
     }
 
     @Override
     protected int getImageSizeY() {
-        return 299;
+        return 224;
     }
 
     @Override
     protected int getNumBytesPerChannel() {
-        // a 32bit float value requires 4 bytes
-        return 4;
+        return 4; // Float.SIZE / Byte.SIZE;
     }
 
     @Override
     protected void addPixelValue(int pixelValue) {
-        imgData.putFloat((((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
-        imgData.putFloat((((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
-        imgData.putFloat(((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+        imgData.putFloat(((pixelValue >> 16) & 0xFF) / 255.f);
+        imgData.putFloat(((pixelValue >> 8) & 0xFF) / 255.f);
+        imgData.putFloat((pixelValue & 0xFF) / 255.f);
     }
 
     @Override
@@ -74,8 +68,7 @@ public class ImageClassifierImageNet extends ImageClassifier {
 
     @Override
     protected float getNormalizedProbability(int labelIndex) {
-        // TODO the following value isn't in [0,1] yet, but may be greater. Why?
-        return getProbability(labelIndex);
+        return labelProbArray[0][labelIndex];
     }
 
     @Override
